271be5d1d1691b62aeab20e0094d2a08c4ece825
John Rexter Flores <johnrexter.flores@gmail.com>
2021-02-10 00:21:18 +0800

    Create offer log model and migration

---
+++ database/migrations/2021_02_09_162030_create_offer_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOfferLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offer_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('date')->nullable();
+            $table->string('person')->nullable();
+            $table->string('units')->nullable();
+            $table->string('amount')->nullable();
+            $table->string('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offer_logs');
+    }
+}
